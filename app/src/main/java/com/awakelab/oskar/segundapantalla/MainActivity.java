885dc90cdd956d0964961574c2a8b7de748b0f32
OskarUno <oorellana80@gmail.com>
2023-06-26 02:08:57 -0400

    Se agregaron los log, para el ciclo de vida de app. Se debe filtrar por la palabra "actividad" el el Logcat para que muestre claramente la secuencia del ciclo de vida.

--- app/src/main/java/com/awakelab/oskar/segundapantalla/MainActivity.java
+++ app/src/main/java/com/awakelab/oskar/segundapantalla/MainActivity.java
@@ -1,19 +1,53 @@
 package com.awakelab.oskar.segundapantalla;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private static  final String TAG = "Actividad 1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initListeners();
+        Log.d(TAG, "onCreate ------------------------------");
+       initListeners();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart ------------------------------");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume ------------------------------");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart ------------------------------");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause ------------------------------");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop ------------------------------");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy ------------------------------");
     }
 
     private void initListeners() {
@@ -24,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent explicitIntent = new Intent(MainActivity.this, Pantalla2.class);
                 startActivity(explicitIntent);
-
             }
         });
     }
--- app/src/main/java/com/awakelab/oskar/segundapantalla/Pantalla2.java
+++ app/src/main/java/com/awakelab/oskar/segundapantalla/Pantalla2.java
@@ -5,19 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class Pantalla2 extends AppCompatActivity {
 
+public class Pantalla2 extends AppCompatActivity {
+    private static  final String TAG = "Actividad 2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
+        Log.d(TAG, "onCreate ------------------------------");
         initListeners();
     }
 
-    private void initListeners() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart ------------------------------");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume ------------------------------");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart ------------------------------");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause ------------------------------");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop ------------------------------");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy ------------------------------");
+    }
+
+        private void initListeners() {
         Button btnLink = findViewById(R.id.btnLink);
         Button btnVolver = findViewById(R.id.btnVolver);
         btnLink.setOnClickListener(new View.OnClickListener() {
